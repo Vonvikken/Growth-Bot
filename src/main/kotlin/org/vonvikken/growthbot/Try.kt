ac922760f81408bd1ef8f1b9522d40a8cb245f37
Vincenzo Stornanti <48057622+Vonvikken@users.noreply.github.com>
2022-06-09 23:07:13 +0200

    Add methods to Try class.

--- src/main/kotlin/org/vonvikken/growthbot/Try.kt
+++ src/main/kotlin/org/vonvikken/growthbot/Try.kt
@@ -2,12 +2,16 @@ package org.vonvikken.growthbot
 
 internal sealed class Try<T> {
     companion object {
+
         operator fun <T> invoke(block: () -> T): Try<T> = try {
             Success(block())
         } catch (exc: Exception) {
             Error(exc)
         }
     }
+
+    internal class Success<T>(val value: T) : Try<T>()
+    internal class Error<T>(val error: Exception) : Try<T>()
 
     @Suppress("IMPLICIT_CAST_TO_ANY")
     override fun toString(): String = when (this) {
@@ -17,9 +21,13 @@ internal sealed class Try<T> {
 }
 
 internal fun <T> Try<T>.orElseGet(default: T): T = when (this) {
-    is Success -> value
-    is Error -> default
+    is Try.Success -> value
+    is Try.Error -> default
 }
 
-internal class Success<T>(val value: T) : Try<T>()
-internal class Error<T>(val error: Exception) : Try<T>()
+internal fun <T> Try<T>.successOrNull(): T? = (this as? Try.Success)?.value
+
+internal fun <T, U> Try<T>.manage(onSuccess: (T) -> U, onError: (Try.Error<T>) -> U): U = when (this) {
+    is Try.Success -> onSuccess(this.value)
+    is Try.Error -> onError(this)
+}
